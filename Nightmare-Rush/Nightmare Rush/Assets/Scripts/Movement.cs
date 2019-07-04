35546e4c381d93a39de26d6c9a3aa116b1488597
Kumuditha Perera <kumudithaperera@gmail.com>
2019-07-04 09:23:58 +0530

    basic movement

---
+++ Nightmare-Rush/Nightmare Rush/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected joyButton joyButton;
+
+    protected bool jump;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joyButton = FindObjectOfType<joyButton>();
+    }
+
+    
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(joystick.Horizontal * 5f, rigidBody.velocity.y, joystick.Vertical * 5f);
+
+        if (!jump && joyButton.pressed)
+        {
+            jump = true;
+            rigidBody.velocity += Vector3.up * 2f;
+        }
+
+        if(jump && !joyButton.pressed)
+        {
+            jump = false;
+        }
+    }
+
+}
---
+++ Nightmare-Rush/Nightmare Rush/Assets/Scripts/joyButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class joyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool pressed;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+}
